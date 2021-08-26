e34981483dc8bad12e1752dd934ee85ace0f1bdb
Guilherme Santiago <santiagoguilhermecosta@gmail.com>
2021-08-26 10:46:04 -0300

    Criado entidades e migrations

---
+++ SalesMVC/Data/SalesMVCContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SalesMVC.Models;
+
+namespace SalesMVC.Data
+{
+    public class SalesMVCContext : DbContext
+    {
+        public SalesMVCContext (DbContextOptions<SalesMVCContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Department> Department { get; set; }
+
+        public DbSet<Seller> Seller { get; set; }
+
+        public DbSet<SalesRecord> SalesRecords { get; set; }
+    }
+}
