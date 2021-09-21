9ecdd16c4fe3a06118c742b963aaf30c785aabf1
Guilherme Santiago <santiagoguilhermecosta@gmail.com>
2021-09-21 17:41:06 -0300

    Seller details e eager loading

--- SalesMVC/Controllers/SellersController.cs
+++ SalesMVC/Controllers/SellersController.cs
@@ -60,5 +60,26 @@ namespace SalesMVC.Controllers
             _sellerService.Remove(id);
             return RedirectToAction(nameof(Index));
         }
+
+        public IActionResult Details(int? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            var obj = _sellerService.FindById(id.Value);
+            if (obj == null)
+                return NotFound();
+
+            return View(obj);
+        }
+        /*
+        [HttpGet]
+        [ValidateAntiForgeryToken]
+        public IActionResult Details(int id)
+        {
+            _sellerService.Remove(id);
+            return RedirectToAction(nameof(Index));
+        }
+        */
     }
 }
