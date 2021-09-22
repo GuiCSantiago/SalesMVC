1d18f7702fa3182b822ac71c5cd8d7aef00820a0
Guilherme Santiago <santiagoguilhermecosta@gmail.com>
2021-09-22 13:58:44 -0300

    Validações

--- SalesMVC/Models/Seller.cs
+++ SalesMVC/Models/Seller.cs
@@ -10,16 +10,21 @@ namespace SalesMVC.Models
     {
         public int Id { get; set; }
 
+        [Required(ErrorMessage = "{0} required!")]
+        [StringLength(100, MinimumLength = 10, ErrorMessage = "({0}) size should be between {1} and {2}")]
         public string Name { get; set; }
 
+        [Required(ErrorMessage = "{0} required!")]
         [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
 
+        [Required(ErrorMessage = "{0} required!")]
         [Display(Name = "Bith Date")]
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime BirthDate { get; set; }
 
+        [Required(ErrorMessage = "{0} required!")]
         [Display(Name = "Base Salary")]
         [DisplayFormat(DataFormatString = "{0:F2}")]
         public double BaseSalary { get; set; }
