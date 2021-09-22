e0b3edce309176d39c128ab31e5b2258b06feb52
Guilherme Santiago <santiagoguilhermecosta@gmail.com>
2021-09-22 12:20:28 -0300

    Retorno de pagina de erro customizada

--- SalesMVC/Models/ViewModels/ErrorViewModel.cs
+++ SalesMVC/Models/ViewModels/ErrorViewModel.cs
@@ -5,6 +5,8 @@ namespace SalesMVC.Models.ViewModels
     {
         public string RequestId { get; set; }
 
+        public string Message { get; set; }
+
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
 }
